1e62cc657af982a9f6f24b0d321e7384baa881da
Pepe García <pgarciag93@gmail.com>
2020-09-15 20:19:42 +0200

    Autogenerated from nuxt-content-blog :tada:

---
+++ pages/_permalink.vue
@@ -0,0 +1,40 @@
+<template>
+<article>
+    <Post v-slot="{ post, author, error }" :permalink="$route.params.permalink">
+      <div v-if="error.code">
+        No post found
+      </div>
+      <article v-else class="max-w-4xl w-full mx-auto">
+        <div class="relative">
+          <img class="flex-1 h-64 w-full object-cover" :src="post.image" :alt="post.title" />
+          <AuthorAvatar
+            :author="author"
+            class="absolute"
+            :style="{ bottom: '1rem', right: '1.8rem' }"
+          />
+        </div>
+
+        <div class="mt-8">
+          <h1 class="mt-2 text-4xl font-medium leading-7">{{ post.title }}</h1>
+          <div class="mt-1">
+            <span class="text-sm font-medium text-gray-700">{{ post.readingTime }}</span>
+            <span>|</span>
+            <time class="text-sm font-medium text-gray-700" :datetime="post.createdAt">
+              {{ post.createdAt }}
+            </time>
+          </div>
+
+          <div class="mt-6">
+            <nuxt-content :document="post" class="prose sm:prose lg:prose-lg xl:prose-xl" />
+          </div>
+        </div>
+      </article>
+    </Post>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ViewPost',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <PostList v-slot="{ posts }">
+    <div class="grid gap-12 lg:grid-cols-2">
+      <ArticleListItem v-for="post in posts" :key="post.path" :post="post" />
+    </div>
+  </PostList>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
